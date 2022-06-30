f6144da08e7cd3ea1910c1b92b537aec4fe0a6e1
Joshua Cook <jcook071990@gmail.com>
2022-06-30 18:49:12 -0400

    added loops

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-let val = ""
-if (confirm("Click OK if you love Video Games!")) {
-    val = "Game ON!";
-}
-else {
-    val = "Game OVER..."
+function askUser(){
+    let userGame = ""
+    if (confirm("Click OK if you love Video Games!")) {
+        userGame = "Game ON!";
+    }
+    else {
+        userGame = "Game OVER..."
+    }
+    return userGame;
 }
 
-let question = prompt("What is your favorite Video Game?");
-if (question != null) {
-    document.getElementById("button").innerHTML =
-    "Ah yes, " + question + " that is an excellent game.";
+function userFavGame(){
+    let question = prompt("What is your favorite Video Game?");
+    let print = '';
+
+    if (question != null) {
+        print = "Ah yes, that is an excellent game.";
+    }
+    else {
+        print = "Try again";
+    }
+
+    return print;
 }
+
+function numberOfGames() {
+    let gameTotal = prompt("How many video games do you own?");
+    let output = '';
+
+    for(let i = 0; i < gameTotal; i++) {
+        output += "<img class='shwing' src='https://www.1addicts.com/forums/attachment.php?attachmentid=2048195&stc=1&d=1557196382'/>";
+    }
+
+    return document.write(output);
+}
+
 
 function confirmFunction(){
     confirm("Click OK to confirm");
 }
+
+askUser();
+// userFavGame();
+confirmFunction();
