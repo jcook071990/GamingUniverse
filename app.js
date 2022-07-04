a14c6ac98929da8d8e9fd2e23dce56ad1a2b80f7
Joshua Cook <jcook071990@gmail.com>
2022-07-04 14:55:37 -0400

    messing with css

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-function askUser(){
-    let userGame = ""
-    if (confirm("Click OK if you love Video Games!")) {
-        userGame = "Game ON!";
-    }
-    else {
-        userGame = "Game OVER..."
-    }
-    return userGame;
-}
+// function askUser(){
+//     let userGame = ""
+//     if (confirm("Click OK if you love Video Games!")) {
+//         userGame = "Game ON!";
+//     }
+//     else {
+//         userGame = "Game OVER..."
+//     }
+//     return userGame;
+// }
 
-function userFavGame(){
-    let question = prompt("What is your favorite Video Game?");
-    let print = '';
+// function userFavGame(){
+//     let question = prompt("What is your favorite Video Game?");
+//     let print = '';
 
-    if (question != null) {
-        print = "Ah yes, that is an excellent game.";
-    }
-    else {
-        print = "Try again";
-    }
+//     if (question != null) {
+//         print = "Ah yes, that is an excellent game.";
+//     }
+//     else {
+//         print = "Try again";
+//     }
+//     return print;
+// }
 
-    return print;
-}
+// function numberOfGames() {
+//     let gameTotal = prompt("How many video games do you own?");
+//     let output = '';
 
-function numberOfGames() {
-    let gameTotal = prompt("How many video games do you own?");
-    let output = '';
-
-    for(let i = 0; i < gameTotal; i++) {
-        output += "<img class='shwing' src='https://www.1addicts.com/forums/attachment.php?attachmentid=2048195&stc=1&d=1557196382'/>";
-    }
-
-    return document.write(output);
-}
+//     for(let i = 0; i < gameTotal; i++) {
+//         output += "<img class='shwing' src='https://www.1addicts.com/forums/attachment.php?attachmentid=2048195&stc=1&d=1557196382'/>";
+//     }
+//     return document.write(output);
+// }
 
 
-function confirmFunction(){
-    confirm("Click OK to confirm");
-}
+// function confirmFunction(){
+//     confirm("Click OK to confirm");
+// }
 
-askUser();
-// userFavGame();
-confirmFunction();
+// askUser();
+// // userFavGame();
+// confirmFunction();
